84e9cf3821db7cc64a84c2372b9d1112d8dcbb54
Douglas-Gray <douglasgray@hotmai.co.uk>
2022-04-30 21:13:41 +0100

    added oscillation option to graph

--- input.js
+++ input.js
@@ -8,14 +8,37 @@ function renderInput(){
     var x2input = document.getElementById("x2").value;
     var time = document.getElementById("time").value; 
     var velocity = parseInt(document.getElementById("velocity").value); 
+    
+    var focus = document.getElementById("focus").checked; 
+
+    //var velocity = 0; 
 
     let x1 = 0;
     let x2 = 0; 
     graphData.push({"x":0, "y":x1, "dataSet":1});
     graphData.push({"x":0, "y":x2, "dataSet":2});
 
+    var countup = false; 
+
     let i = 1;
     while (i <= time) {
+
+        if (focus == true){
+            if (x1 >= 1){countup = false;}
+            else if (x1 <= -1){countup = true;}
+
+            if (countup == false){
+                velocity -= 0.5;
+            }
+            else if (countup == true){
+                velocity += 0.5;
+            }
+        }
+
+        console.log(velocity);
+        console.log(x1);
+        console.log(countup);
+
 
         x1 += x1input * velocity;
         x2 += x2input * velocity;
